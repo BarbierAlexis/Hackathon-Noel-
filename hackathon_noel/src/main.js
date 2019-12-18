383127fce930b769acea62543d89e5dd1033a5ee
Alexis Barbier <barbier-alexis@hotmail.fr>
2019-12-18 22:25:44 +0100

    Fixed bug

--- hackathon_noel/src/main.js
+++ hackathon_noel/src/main.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
-<<<<<<< HEAD
 import axios from 'axios'
 
 Vue.use(BootstrapVue)
 Vue.use(axios)
-=======
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
->>>>>>> geoloc
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
